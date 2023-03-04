5497ce08199bf0696230ae58f6cdd34c8cebc2e6
Nikhil539 <84114368+Nikhil539@users.noreply.github.com>
2023-03-04 09:35:52 +0530

    Add files via upload

---
+++ rakesh p2.py
@@ -0,0 +1,27 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:/Users/student/rakesh python.1.py =================
+9
+>>> 
+================= RESTART: C:/Users/student/rakesh python.1.py =================
+9
+>>> 
+================= RESTART: C:/Users/student/rakesh python.1.py =================
+9 -1 20
+>>> 
+================= RESTART: C:/Users/student/rakesh python.1.py =================
+9 -1 20 0.8
+>>> 
+================= RESTART: C:/Users/student/rakesh python.1.py =================
+enter n1:8
+enter n2:8
+Traceback (most recent call last):
+  File "C:/Users/student/rakesh python.1.py", line 3, in <module>
+    sum=n1+n2
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> 
+================= RESTART: C:/Users/student/rakesh python.1.py =================
+enter n1:5
+enter n2:5
+10

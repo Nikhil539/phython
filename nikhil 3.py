3683d538858418c0dcee90225833e11ca6c0a977
Nikhil539 <84114368+Nikhil539@users.noreply.github.com>
2023-03-04 10:11:02 +0530

    Add files via upload

---
+++ nikhil 3.py
@@ -0,0 +1,10 @@
+x=int(input("enter x number"))
+y=int(input("enter y number"))
+z=int(input("enter z number"))
+if x>y and x>z:
+    print("x is largest number")
+elif y>x and y>z:
+    print("y is lsrgest number")
+else:
+    print("z is largest number")
+    
